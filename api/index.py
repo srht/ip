e64e6124fbc051c7b4eb159a13fcbb27414c8b6c
Serhat Köroğlu <srhtkrgl@outlook.com>
2023-04-10 16:07:38 +0300

    fsds

--- api/index.py
+++ api/index.py
@@ -3,5 +3,6 @@ class handler(BaseHTTPRequestHandler):
   def do_GET(self):
     self.send_response(200)
     self.end_headers()
+    self.wfile.write('sfdas')
     self.wfile.write(self.headers.get('x-forwarded-for').encode())
     return
